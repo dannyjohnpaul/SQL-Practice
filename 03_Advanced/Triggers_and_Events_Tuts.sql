7394ca1890a7ee75f57db11db2efe83527a41e58
dannyjohnpaul <dannyjohnson.dev@gmail.com>
2025-05-12 18:21:38 -0400

    Added more Trigger and Events queries

--- 03_Advanced/Triggers_and_Events_Tuts.sql
+++ 03_Advanced/Triggers_and_Events_Tuts.sql
@@ -1,3 +1,26 @@
+-- Triggers and Events
+
+SELECT *
+FROM employee_demographics;
+
+SELECT *
+FROM employee_salary;
+
+
+DELIMITER $$
+CREATE TRIGGER employee_insert
+	AFTER INSERT ON employee_salary
+    FOR EACH ROW
+BEGIN
+	INSERT INTO employee_demographics (employee_id, first_name, last_name)
+    VALUES(NEW.employee_id, NEW.first_name, NEW.last_name);
+END $$
+DELIMITER ;
+
+INSERT INTO employee_salary (employee_id, first_name, last_name, occupation, salary, dept_id)
+VALUES(13, 'Jean-Ralphio', 'Saperstein', 'Entertainment 720 CEO', 100000, NULL);
+
+
 -- Events
 
 SELECT *
@@ -9,8 +32,11 @@ CREATE EVENT delete_retirees
 ON SCHEDULE EVERY 30 SECOND
 DO
 BEGIN
-        DELETE
-        FROM employee_demographics
-        WHERE age >= 60;
+	DELETE
+	FROM employee_demographics
+	WHERE age >= 60;
 END $$
 DELIMITER ;
+
+
+
