c4022b8e364430ea6bf2b6a5f47949d7b38c9336
dannyjohnpaul <dannyjohnson.dev@gmail.com>
2025-05-16 23:18:35 -0400

    Standardized industry, country, and date formats in layoffs_staging2

--- Projects/Data_Cleaning.sql
+++ Projects/Data_Cleaning.sql
@@ -83,6 +83,27 @@ UPDATE layoffs_staging2
 SET company = TRIM(company);
 
 
+SELECT DISTINCT industry
+FROM layoffs_staging2;
+
+UPDATE layoffs_staging2
+SET industry = 'Crypto'
+WHERE industry LIKE 'Crypto%';
+
+SELECT DISTINCT country, TRIM(TRAILING '.' FROM country)
+FROM layoffs_staging2
+ORDER BY 1;
+
+UPDATE layoffs_staging2
+SET country = TRIM(TRAILING '.' FROM country)
+WHERE country LIKE 'United States%';
 
 
+SELECT `date`
+FROM layoffs_staging2;
 
+UPDATE layoffs_staging2
+SET `date` = STR_TO_DATE(`date`, '%m/%d/%Y');
+
+ALTER TABLE layoffs_staging2
+MODIFY COLUMN `date` DATE;
