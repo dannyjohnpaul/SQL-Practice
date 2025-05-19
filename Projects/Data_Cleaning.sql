a278d7c350054070b3dc1c86e9b8ec686e80f54f
dannyjohnpaul <dannyjohnson.dev@gmail.com>
2025-05-19 03:38:13 -0400

    Added logic to update industry values for companies with missing data in layoffs_staging2

--- Projects/Data_Cleaning.sql
+++ Projects/Data_Cleaning.sql
@@ -118,3 +118,22 @@ SELECT *
 FROM layoffs_staging2
 WHERE industry IS NULL
 OR industry = '';
+
+SELECT *
+FROM layoffs_staging2
+WHERE company = 'Airbnb';
+
+SELECT t1.industry, t2.industry
+FROM layoffs_staging2 t1
+JOIN layoffs_staging2 t2
+	ON t1.company = t2.company
+WHERE (t1.industry IS NULL or t1.industry = '')
+AND t2.industry IS NOT NULL;
+
+UPDATE layoffs_staging2 t1
+JOIN layoffs_staging2 t2
+	ON t1.company = t2.company
+SET t1.industry = t2.industry
+WHERE t1.industry IS NULL
+AND t2.industry IS NOT NULL; 
+
